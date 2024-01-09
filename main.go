4abff61c439cdfe9afc75151a682db3fef577ed2
ashutoshbr <ashutoshbr@proton.me>
2024-01-09 11:48:04 +0545

    GORM and PostgresDB setup

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"github.com/ashutoshbr/Fiber-GORM/user"
 	"github.com/gofiber/fiber/v2"
 )
 
 func home(c *fiber.Ctx) error {
 	return c.SendString("Namaste")
 }
+
+func Routers(app *fiber.App) {
+	app.Get("/users", user.GetUsers)
+	app.Post("/user", user.SaveUser)
+}
+
 func main() {
+	user.InitialMigration()
 	app := fiber.New()
 	app.Get("/", home)
+	Routers(app)
 	app.Listen(":8000")
 }
